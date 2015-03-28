1dc122d63d99e745c3b1e27cb181a46b9eff484f
nmobassir <mr.naib@gmail.com>
2015-03-28 16:53:50 -0400

    Javascript
    js

--- BU-Hacks-Project/src/bu/hacks/project/BUHacksProject.java
+++ BU-Hacks-Project/src/bu/hacks/project/BUHacksProject.java
@@ -5,6 +5,9 @@
  */
 package bu.hacks.project;
 
+import javax.sound.sampled.*;
+import java.io.*;
+
 /**
  *
  * @author Naib
@@ -19,8 +22,20 @@ public class BUHacksProject {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
         // TODO code application logic here
+        File file = new File("ihaveadream.mp3");
+        AudioInputStream in = AudioSystem.getAudioInputStream(file);
+        AudioInputStream din = null;
+        AudioFormat baseFormat = in.getFormat();
+        AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+                baseFormat.getSampleRate(),
+                16,
+                baseFormat.getChannels(),
+                baseFormat.getChannels() * 2,
+                baseFormat.getSampleRate(),
+                false);
+        din = AudioSystem.getAudioInputStream(decodedFormat, in);
     }
-    
+
 }
