3a4dcaa92e4df0de97ab95094bc981c479cf5af0
Daniel Gorelick <dqgorelick@gmail.com>
2015-03-28 21:38:22 -0400

    semantria implementation

--- BU-Hacks-Project/website/js/main.js
+++ BU-Hacks-Project/website/js/main.js
@@ -1,4 +1,3 @@
-
 var langs =
 [['Afrikaans',       ['af-ZA']],
  ['Bahasa Indonesia',['id-ID']],
@@ -229,5 +228,134 @@ function showButtons(style) {
     return;
   }
   current_style = style;
+}
+
+
+/************************************
+SEMANTRIA CODE
+*************************************/
+
+var semantria = function(){
+
+  var consumerKey = "570d49f2-d717-4dd8-ad1f-ed64d98697f3";
+  var consumerSecret = "4ec66d89-40a6-4ebd-965f-669d6a8a3e4c";
+
+  function log(message) {
+    $("#speech_rating").append(message + "<br/>");
+  }
+  console.log(final_transcript);
+  function getInitialText() {
+    var result = [];
+    //final text from the recording added here
+    result.push(final_transcript);
+    return result;
+  }
+  window.runTestApp = function() {
+
+  }
+
+  function processResponse(analyticData) {
+    for(var i=0, data;data=analyticData[i];i++) {
+      // Printing of document sentiment score
+      log(SemantriaActiveSession.tpl("Document {id}. Sentiment score: {sentiment_score}", data));
+      // Printing of document themes
+      log("<div style='margin-left: 15px;'/>Document themes:");
+      if (data.themes) {
+        for(var j=0, theme; theme=data.themes[j]; j++) {
+          log(SemantriaActiveSession.tpl("<div style='margin-left: 30px;'/>{title} (sentiment: {sentiment_score})", theme));
+        }
+      } else {
+        log("<div style='margin-left: 30px;'/>No themes were extracted for this text");
+      }
+
+      // Printing of document entities
+      log("<div style='margin-left: 15px;'/>Entities:");
+      if (data['entities']) {
+        for(var j=0, entity; entity=data['entities'][j]; j++) {
+          log(SemantriaActiveSession.tpl(
+            "<div style='margin-left: 30px;'/>{title} : {entity_type} (sentiment: {sentiment_score})", entity
+          ));
+        }
+      } else {
+        log("<div style='margin-left: 30px;'/>No entities were extracted for this text");
+      }
+      
+      // Printing of document entities
+      log("<div style='margin-left: 15px;'/>Topics:");
+      if (data.topics) {
+        for(var j=0, topic; topic=data.topics[j]; j++) {
+          log(SemantriaActiveSession.tpl(
+            "<div style='margin-left: 30px;'/>{title} : {type} (strength: {sentiment_score})", topic
+          ));
+        }
+      } else {
+        log("<div style='margin-left: 30px;'/>No topics were extracted for this text");
+      }
+    }
+    function receiveResponse(entitiesCount) {
+      // As Semantria isn't real-time solution you need to wait some time before getting of the processed results
+      // Wait ten seconds while Semantria process queued document
+
+      var analyticData = [];
+      var timeout = setInterval(function() {
+        log("Retrieving your processed results...");
+        // Requests processed results from Semantria service
+        var processedDocuments = SemantriaActiveSession.getProcessedDocuments();
+
+        if (processedDocuments && processedDocuments.length) {
+          analyticData = analyticData.concat(processedDocuments);
+        }
+
+        if(analyticData.length == entitiesCount) {
+          clearInterval(timeout);
+          processResponse(analyticData);
+        } 
+      }, 2000);
+    }
+
+    window.runTestApp = function(){
+      var initialTexts = getInitialText();
+      
+      // session is a global object
+      SemantriaActiveSession = new Semantria.Session(consumerKey, consumerSecret, "MakeBU");
+      SemantriaActiveSession.override({
+        onError: function() {
+          console.warn("onError:");
+          console.warn(arguments);
+        },
+
+        onRequest: function() {
+          console.log("onRequest:");
+          console.log(arguments);
+        },
+
+        onResponse: function() {
+          console.log("onResponse:");
+          console.log(arguments);
+        },
+
+        onAfterResponse: function() {
+          console.log("onAfterResponse:");
+          console.log(arguments);
+        }
+      });
+        for(var i=0,text; text=initialTexts[i]; i++) {
+          // Creates a sample document which need to be processed on Semantria
+          var id = Math.floor(Math.random() * 10000000);
+          // Queues document for processing on Semantria service
+          var status = SemantriaActiveSession.queueDocument({
+            id: id,
+            text: text
+          });
+            
+          // Check status from Semantria service
+          if (status == 202) {
+            log("Document# " + id + " queued successfully");
+          }
+        }
+
+        receiveResponse(initialTexts.length);
+      }
+  }
 }
 
